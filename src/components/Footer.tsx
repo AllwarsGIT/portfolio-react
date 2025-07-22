628e0651553b1cdfda76f6bdc15e96220d3ef710
DESKTOP-8CPSTED\Álvaro Sánchez Torre <allwarsps@gmail.com>
2025-07-22 16:08:16 +0200

    Minor footer fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <div className="bg-card text-graySecondary text-center">
-      <p className="text-sm md:text-base py-5">
+      <p className="text-xs md:text-base py-5">
         © {new Date().getFullYear()} Álvaro Sánchez. Todos los derechos reservados.
       </p>
     </div>
