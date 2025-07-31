c74f4bcf7ab191722ed87bec97b33fe666060978
DESKTOP-8CPSTED\Álvaro Sánchez Torre <allwarsps@gmail.com>
2025-07-31 14:41:59 +0200

    Minor comments(End of V2)

--- src/hooks/useIntersectionObserver.tsx
+++ src/hooks/useIntersectionObserver.tsx
@@ -1,27 +1,27 @@
 import { useEffect, useState, useRef } from 'react';
 
 export const useIntersectionObserver = (options = {}) => {
-    const [isIntersecting, setIsIntersecting] = useState(false);
+    const [isIntersecting, setIsIntersecting] = useState(false); // Estados para saber si el elemento está intersectando o si ya lo ha hecho
     const [hasIntersected, setHasIntersected] = useState(false)
-    const elementRef = useRef(null);
+    const elementRef = useRef(null); // Referencia al elemento a observar
 
     useEffect(() => {
-        const element = elementRef.current;
+        const element = elementRef.current; // Obtengo el elemento de la referencia o nada
         if (!element) return;
 
-        const observer = new IntersectionObserver(([entry]) => {
-            setIsIntersecting(entry.isIntersecting);
+        const observer = new IntersectionObserver(([entry]) => { // Uso intersectionObserver para observar el elemento
+            setIsIntersecting(entry.isIntersecting); // Actualizo el estado de isIntersecting
 
-            if (entry.isIntersecting && !hasIntersected) {
+            if (entry.isIntersecting && !hasIntersected) { // Actualizo hasIntersected la primera vez que intersecta
                 setHasIntersected(true);
             }
         }, options);
 
-        observer.observe(element);
+        observer.observe(element); // Inicio al observador con el elemento
         
-        return () => observer.disconnect();
+        return () => observer.disconnect(); // Limpio useEffect para evitar leaks de memoria(recordar en futuros hooks)
 
     }, []);
 
-    return { elementRef, isIntersecting, hasIntersected };
+    return { elementRef, isIntersecting, hasIntersected }; // Retorno de la referencia y los estados
 }
