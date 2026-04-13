07e4a8ec8e8daf8ba3f0a3667cba1feb04598bb9
DESKTOP-8CPSTED\Álvaro Sánchez Torre <allwarsps@gmail.com>
2026-04-14 00:26:36 +0200

    vercel analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Projects from "../components/Projects"
 import AboutMe from "../components/AboutMe"
 // import ContactMe from "../components/ContactMe"
 import Footer from "../components/Footer"
+import { Analytics } from "@vercel/analytics/next"
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
         <Hero />
         <Projects />
         <AboutMe />
+        <Analytics />
         {/* <ContactMe /> */}
       </main>
       <Footer />
